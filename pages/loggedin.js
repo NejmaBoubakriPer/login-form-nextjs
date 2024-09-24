1ab30e62fce4014b6cc34caadb0b693a5bf9e283
NejmaBoubakriPer <132521008+NejmaBoubakriPer@users.noreply.github.com>
2024-09-24 12:59:12 +0200

    Update loggedin.js

--- pages/loggedin.js
+++ pages/loggedin.js
@@ -2,6 +2,7 @@ export default function LoggedIn() {
   return (
     <div style={{ textAlign: "center", marginTop: "50px" }}>
       <h1>You are logged in</h1>
+      <a href="/.auth/logout" style={{ marginTop: "20px", display: "block" }}>Sign out</a>
     </div>
   );
 }
